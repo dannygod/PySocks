c26a9f85b7f6a627c8e11c1ca6b9481eff558860
dannygod <dannygodii@gmail.com>
2014-04-27 13:54:42 +0800

    format codes
    Signed-off-by: dannygod <dannygodii@gmail.com>

--- PySocks/local.py
+++ PySocks/local.py
@@ -29,181 +29,197 @@ import struct
 import os
 import logging
 try:
-    import gevent
-    import gevent.monkey
-    gevent.monkey.patch_all(dns=gevent.version_info[0] >= 1)
+  import gevent
+  import gevent.monkey
+  gevent.monkey.patch_all(dns=gevent.version_info[0] >= 1)
 except ImportError:
-    gevent = None
-    sys.stdout.write('warning: gevent not found, using threading instead')
+  gevent = None
+  sys.stdout.write('warning: gevent not found, using threading instead')
 try:
-    import encrypt
-    import utils
+  import encrypt
+  import utils
 except ImportError:
-    sys.path.append(os.path.join(os.path.dirname(sys.argv[0])))
-    import encrypt
-    import utils
+  sys.path.append(os.path.join(os.path.dirname(sys.argv[0])))
+  import encrypt
+  import utils
 try:
-    import ctypes
+  import ctypes
 except ImportError:
-    ctypes = None
+  ctypes = None
 
 logging = sys.modules['logging'] = utils.Logging('logging')
 common = utils.Common()
 
 
 def send_all(sock, data):
-    bytes_sent = 0
-    while True:
-        r = sock.send(data[bytes_sent:])
-        if r < 0:
-            return r
-        bytes_sent += r
-        if bytes_sent == len(data):
-            return bytes_sent
+  bytes_sent = 0
+  while True:
+    r = sock.send(data[bytes_sent:])
+    if r < 0:
+      return r
+    bytes_sent += r
+    if bytes_sent == len(data):
+      return bytes_sent
 
 
 class ThreadingTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
-    allow_reuse_address = True
+  allow_reuse_address = False
 
 
 class Socks5Server(SocketServer.StreamRequestHandler):
-    def handle_tcp(self, sock, remote):
-        try:
-            fdset = [sock, remote]
-            while True:
-                r, w, e = select.select(fdset, [], [])
-                if sock in r:
-                    data = self.encrypt(sock.recv(4096))
-                    if len(data) <= 0:
-                        break
-                    result = send_all(remote, data)
-                    if result < len(data):
-                        raise Exception('failed to send all data')
+  timeout = common.TIMEOUT
 
-                if remote in r:
-                    data = self.decrypt(remote.recv(4096))
-                    if len(data) <= 0:
-                        break
-                    result = send_all(sock, data)
-                    if result < len(data):
-                        raise Exception('failed to send all data')
-        finally:
-            sock.close()
-            remote.close()
+  def handle_tcp(self, sock, remote):
+    try:
+      fdset = [sock, remote]
+      while True:
+        r, w, e = select.select(fdset, [], [])
+        if sock in r:
+          data = self.encrypt(sock.recv(4096))
+          if len(data) <= 0:
+            break
+          result = send_all(remote, data)
+          if result < len(data):
+            raise Exception('failed to send all data to local')
+        
+        if remote in r:
+          data = self.decrypt(remote.recv(4096))
+          if len(data) <= 0:
+            break
+          result = send_all(sock, data)
+          if result < len(data):
+            raise Exception('failed to send all data to remote')
+    finally:
+      sock.close()
+      remote.close()
 
-    def encrypt(self, data):
-        return self.encryptor.encrypt(data)
+  def encrypt(self, data):
+    return self.encryptor.encrypt(data)
 
-    def decrypt(self, data):
-        return self.encryptor.decrypt(data)
+  def decrypt(self, data):
+    return self.encryptor.decrypt(data)
 
-    def send_encrypt(self, sock, data):
-        sock.send(self.encrypt(data))
+  def send_encrypt(self, sock, data):
+    sock.send(self.encrypt(data))
 
-    def handle(self):
-        try:
-            self.encryptor = encrypt.Encryptor(common.SOCKS5_PASSWORD, common.SOCKS5_ENCRYPT_METHOD)
-            sock = self.connection
-            sock.recv(262)
-            sock.send("\x05\x00")
-            data = self.rfile.read(4) or '\x00' * 4
-            mode = ord(data[1])
-            if mode != 1:
-                logging.warn('mode != 1')
-                return
-            addrtype = ord(data[3])
-            addr_to_send = data[3]
-            if addrtype == 1:
-                addr_ip = self.rfile.read(4)
-                addr = socket.inet_ntoa(addr_ip)
-                addr_to_send += addr_ip
-            elif addrtype == 3:
-                addr_len = self.rfile.read(1)
-                addr = self.rfile.read(ord(addr_len))
-                addr_to_send += addr_len + addr
-            elif addrtype == 4:
-                addr_ip = self.rfile.read(16)
-                addr = socket.inet_ntop(socket.AF_INET6, addr_ip)
-                addr_to_send += addr_ip
-            else:
-                logging.warn('addr_type not support')
-                # not support
-                return
-            addr_port = self.rfile.read(2)
-            addr_to_send += addr_port
-            port = struct.unpack('>H', addr_port)
-            try:
-                reply = "\x05\x00\x00\x01"
-                reply += socket.inet_aton('0.0.0.0') + struct.pack(">H", 2222)
-                self.wfile.write(reply)
-                # reply immediately
-                remote = socket.create_connection((common.SOCKS5_SERVER, common.SOCKS5_SERVER_PORT))
-                self.send_encrypt(remote, addr_to_send)
-                logging.info('connecting %s:%d' % (addr, port[0]))
-            except socket.error, e:
-                logging.warn('%s' % e)
-                return
-            self.handle_tcp(sock, remote)
-        except socket.error, e:
-            logging.warn('%s' % e)
+  def handle(self):
+    try:
+      self.encryptor = encrypt.Encryptor(common.SOCKS5_PASSWORD, common.SOCKS5_ENCRYPT_METHOD)
+      sock = self.connection
+      sock.recv(262)
+      sock.send("\x05\x00")
+      data = self.rfile.read(4) or '\x00' * 4
+      mode = ord(data[1])
+
+      if mode != 1:
+        logging.warn('mode != 1')
+        return
+
+      addrtype = ord(data[3])
+      addr_to_send = data[3]
+
+      if addrtype == 1:
+        addr_ip = self.rfile.read(4)
+        addr = socket.inet_ntoa(addr_ip)
+        addr_to_send += addr_ip
+      elif addrtype == 3:
+        addr_len = self.rfile.read(1)
+        addr = self.rfile.read(ord(addr_len))
+        addr_to_send += addr_len + addr
+      elif addrtype == 4:
+        addr_ip = self.rfile.read(16)
+        addr = socket.inet_ntop(socket.AF_INET6, addr_ip)
+        addr_to_send += addr_ip
+      else:
+        logging.warn('addr_type not support')
+        # not support
+        return
+
+      addr_port = self.rfile.read(2)
+      addr_to_send += addr_port
+      port = struct.unpack('>H', addr_port)
+
+      try:
+        reply = "\x05\x00\x00\x01"
+        reply += socket.inet_aton('0.0.0.0') + struct.pack(">H", 2222)
+        self.wfile.write(reply)
+        # reply immediately
+        remote = socket.create_connection((common.SOCKS5_SERVER,
+                                           common.SOCKS5_SERVER_PORT),
+                                          common.TIMEOUT)
+        self.send_encrypt(remote, addr_to_send)
+
+        logging.info('connecting %s:%d' % (addr, port[0]))
+
+      except (IOError, socket.timeout) as e:
+        logging.error('Error[%s] %s' % (addr, e))
+        return
+
+      self.handle_tcp(sock, remote)
+
+    except (IOError, socket.timeout) as e:
+      logging.error('Errors[%s] %s' % (addr, e))
 
 
 def pre_start():
-    if sys.platform == 'cygwin':
-        logging.critical('cygwin platform is not supported, please download `http://www.python.org/getit/`')
-        sys.exit(-1)
-    if ctypes and os.name == 'nt':
-        ctypes.windll.kernel32.SetConsoleTitleW(u'ShadowSocks v%s' % common.VERSION)
-        if not common.LISTEN_VISIBLE:
-            ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 0)
-        else:
-            ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 1)
+  if sys.platform == 'cygwin':
+    logging.critical('cygwin platform is not supported, please download `http://www.python.org/getit/`')
+    sys.exit(-1)
+  if ctypes and os.name == 'nt':
+    ctypes.windll.kernel32.SetConsoleTitleW(u'ShadowSocks v%s' % common.VERSION)
+    if not common.LISTEN_VISIBLE:
+      ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 0)
+    else:
+      ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 1)
+    
+    blacklist = {
+                    # http://s.weibo.com/weibo/goagent%2520360%2520%25E5%258D%25B8%25E8%25BD%25BD
+                    '360safe' : False, 
+                    # http://s.weibo.com/weibo/goagent%2520qqprotect
+                    'QQProtect' : False, 
+                }
+    softwares = [k for k,v in blacklist.items() if v]
+    if softwares:
+      tasklist = os.popen('tasklist').read().lower()
+      softwares = [x for x in softwares if x.lower()in tasklist]
+      if softwares:
+        error = u'某些安全软件(如 %s)可能和本软件存在冲突，造成 CPU 占用过高。\n如有此现象建议暂时退出此安全软件来继续运行PySocks' % ','.join(softwares)
+        ctypes.windll.user32.MessageBoxW(None, error, u'PySocks 建议', 0)
 
-        blacklist = {
-                        '360safe' : False, # http://s.weibo.com/weibo/goagent%2520360%2520%25E5%258D%25B8%25E8%25BD%25BD
-                        'QQProtect' : False, # http://s.weibo.com/weibo/goagent%2520qqprotect
-                    }
-        softwares = [k for k,v in blacklist.items() if v]
-        if softwares:
-            tasklist = os.popen('tasklist').read().lower()
-            softwares = [x for x in softwares if x.lower()in tasklist]
-            if softwares:
-                error = u'某些安全软件(如 %s)可能和本软件存在冲突，造成 CPU 占用过高。\n如有此现象建议暂时退出此安全软件来继续运行PySocks' % ','.join(softwares)
-                ctypes.windll.user32.MessageBoxW(None, error, u'PySocks 建议', 0)
-                #sys.exit(0)
 
 def main():
-    global __file__
-    __file__ = os.path.abspath(__file__)
-    if os.path.islink(__file__):
-        __file__ = getattr(os, 'readlink', lambda x:x)(__file__)
-    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+  global __file__
+  __file__ = os.path.abspath(__file__)
+  if os.path.islink(__file__):
+    __file__ = getattr(os, 'readlink', lambda x:x)(__file__)
+  os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-    logging.basicConfig(level=logging.DEBUG if common.LISTEN_DEBUGINFO else logging.INFO,
-                        format='%(levelname)s - %(asctime)s %(message)s',
-                        datefmt='[%b %d %H:%M:%S]')
+  logging.basicConfig(level=logging.DEBUG if common.LISTEN_DEBUGINFO else logging.INFO,
+                      format='%(levelname)s - %(asctime)s %(message)s',
+                      datefmt='[%b %d %H:%M:%S]')
 
-    pre_start()
-    sys.stdout.write(common.info())
+  pre_start()
+  sys.stdout.write(common.info())
 
-    encrypt.init_table(common.SOCKS5_PASSWORD, common.SOCKS5_ENCRYPT_METHOD)
+  encrypt.init_table(common.SOCKS5_PASSWORD, common.SOCKS5_ENCRYPT_METHOD)
 
-    try:
-        # not support ipv6 now.
-        #if IPv6:
-        #    ThreadingTCPServer.address_family = socket.AF_INET6
-        server = ThreadingTCPServer((common.LISTEN_IP, common.LISTEN_PORT), Socks5Server)
-        logging.info('starting local at %s:%d' % tuple(server.server_address[:2]))
-        server.serve_forever()
-    except socket.error, e:
-        logging.error('%s' % e)
+  try:
+    # not support ipv6 now.
+    #if IPv6:
+    #    ThreadingTCPServer.address_family = socket.AF_INET6
+    server = ThreadingTCPServer((common.LISTEN_IP, common.LISTEN_PORT), Socks5Server)
+    logging.info('starting local at %s:%d' % tuple(server.server_address[:2]))
+    server.serve_forever()
+  except Exception as e:
+    logging.error('local server failed %s' % e)
+
 
 if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        pass
-    except Exception as e:
-        if ctypes and os.name == 'nt':
-            ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 1)
-        raise
+  try:
+    main()
+  except KeyboardInterrupt:
+    pass
+  except Exception as e:
+    if ctypes and os.name == 'nt':
+      ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 1)
+    raise
--- PySocks/utils.py
+++ PySocks/utils.py
@@ -1,119 +1,134 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-__version__ = '2.0.0'
+__version__ = '2.1.0'
 
 import os
 import sys
 import logging
 import time
 try:
-    import ctypes
+  import ctypes
 except ImportError:
-    ctypes = None
+  ctypes = None
 
 
 class Logging(type(sys)):
-    NOLOGGING = 100
-    CRITICAL = 50
-    FATAL = CRITICAL
-    ERROR = 40
-    WARNING = 30
-    WARN = WARNING
-    INFO = 20
-    DEBUG = 10
-    NOTSET = 0
-    def __init__(self, *args, **kwargs):
-        self.level = self.__class__.INFO
-        if self.level > self.__class__.DEBUG:
-            self.debug = self.dummy
-        self.__write = __write = sys.stderr.write
-        self.isatty = getattr(sys.stderr, 'isatty', lambda:False)()
-        self.__set_error_color = lambda:None
-        self.__set_warning_color = lambda:None
-        self.__reset_color = lambda:None
-        if self.isatty:
-            if os.name == 'nt':
-                SetConsoleTextAttribute = ctypes.windll.kernel32.SetConsoleTextAttribute
-                GetStdHandle = ctypes.windll.kernel32.GetStdHandle
-                self.__set_error_color = lambda:SetConsoleTextAttribute(GetStdHandle(-11), 0x04)
-                self.__set_warning_color = lambda:SetConsoleTextAttribute(GetStdHandle(-11), 0x06)
-                self.__reset_color = lambda:SetConsoleTextAttribute(GetStdHandle(-11), 0x07)
-            elif os.name == 'posix':
-                self.__set_error_color = lambda:__write('\033[31m')
-                self.__set_warning_color = lambda:__write('\033[33m')
-                self.__reset_color = lambda:__write('\033[0m')
-    @classmethod
-    def getLogger(cls, *args, **kwargs):
-        return cls(*args, **kwargs)
-    def basicConfig(self, *args, **kwargs):
-        self.level = kwargs.get('level', self.__class__.INFO)
-        if self.level > self.__class__.DEBUG:
-            self.debug = self.dummy
-        if self.level == self.__class__.NOLOGGING:
-            self.debug = self.dummy
-            self.info = self.dummy
-            self.warning = self.dummy
-            self.error = self.dummy
-            self.critical = self.dummy
-    def log(self, level, fmt, *args, **kwargs):
-        self.__write('%s - - [%s] %s\n' % (level, time.ctime()[4:-5], fmt%args))
-    def dummy(self, *args, **kwargs):
-        pass
-    def debug(self, fmt, *args, **kwargs):
-        self.log('DEBUG', fmt, *args, **kwargs)
-    def info(self, fmt, *args, **kwargs):
-        self.log('INFO', fmt, *args)
-    def warning(self, fmt, *args, **kwargs):
-        self.__set_warning_color()
-        self.log('WARNING', fmt, *args, **kwargs)
-        self.__reset_color()
-    def warn(self, fmt, *args, **kwargs):
-        self.warning(fmt, *args, **kwargs)
-    def error(self, fmt, *args, **kwargs):
-        self.__set_error_color()
-        self.log('ERROR', fmt, *args, **kwargs)
-        self.__reset_color()
-    def exception(self, fmt, *args, **kwargs):
-        self.error(fmt, *args, **kwargs)
-        traceback.print_exc(file=sys.stderr)
-    def critical(self, fmt, *args, **kwargs):
-        self.__set_error_color()
-        self.log('CRITICAL', fmt, *args, **kwargs)
-        self.__reset_color()
+  NOLOGGING = 100
+  CRITICAL = 50
+  FATAL = CRITICAL
+  ERROR = 40
+  WARNING = 30
+  WARN = WARNING
+  INFO = 20
+  DEBUG = 10
+  NOTSET = 0
+
+  def __init__(self, *args, **kwargs):
+    self.level = self.__class__.INFO
+    if self.level > self.__class__.DEBUG:
+      self.debug = self.dummy
+    self.__write = __write = sys.stderr.write
+    self.isatty = getattr(sys.stderr, 'isatty', lambda:False)()
+    self.__set_error_color = lambda:None
+    self.__set_warning_color = lambda:None
+    self.__reset_color = lambda:None
+    if self.isatty:
+      if os.name == 'nt':
+        SetConsoleTextAttribute = ctypes.windll.kernel32.SetConsoleTextAttribute
+        GetStdHandle = ctypes.windll.kernel32.GetStdHandle
+        self.__set_error_color = lambda:SetConsoleTextAttribute(GetStdHandle(-11), 0x04)
+        self.__set_warning_color = lambda:SetConsoleTextAttribute(GetStdHandle(-11), 0x06)
+        self.__reset_color = lambda:SetConsoleTextAttribute(GetStdHandle(-11), 0x07)
+      elif os.name == 'posix':
+        self.__set_error_color = lambda:__write('\033[31m')
+        self.__set_warning_color = lambda:__write('\033[33m')
+        self.__reset_color = lambda:__write('\033[0m')
+
+  @classmethod
+  def getLogger(cls, *args, **kwargs):
+    return cls(*args, **kwargs)
+
+  def basicConfig(self, *args, **kwargs):
+    self.level = kwargs.get('level', self.__class__.INFO)
+    if self.level > self.__class__.DEBUG:
+        self.debug = self.dummy
+    if self.level == self.__class__.NOLOGGING:
+      self.debug = self.dummy
+      self.info = self.dummy
+      self.warning = self.dummy
+      self.error = self.dummy
+      self.critical = self.dummy
+
+  def log(self, level, fmt, *args, **kwargs):
+    self.__write('%s - - [%s] %s\n' % (level, time.ctime()[4:-5], fmt%args))
+
+  def dummy(self, *args, **kwargs):
+    pass
+
+  def debug(self, fmt, *args, **kwargs):
+    self.log('DEBUG', fmt, *args, **kwargs)
+
+  def info(self, fmt, *args, **kwargs):
+    self.log('INFO', fmt, *args)
+
+  def warning(self, fmt, *args, **kwargs):
+    self.__set_warning_color()
+    self.log('WARNING', fmt, *args, **kwargs)
+    self.__reset_color()
+
+  def warn(self, fmt, *args, **kwargs):
+    self.warning(fmt, *args, **kwargs)
+
+  def error(self, fmt, *args, **kwargs):
+    self.__set_error_color()
+    self.log('ERROR', fmt, *args, **kwargs)
+    self.__reset_color()
+
+  def exception(self, fmt, *args, **kwargs):
+    self.error(fmt, *args, **kwargs)
+    traceback.print_exc(file=sys.stderr)
+
+  def critical(self, fmt, *args, **kwargs):
+    self.__set_error_color()
+    self.log('CRITICAL', fmt, *args, **kwargs)
+    self.__reset_color()
 
 
 class Common(object):
-    """Global Config Object"""
+  """Global Config Object"""
+  
+  def __init__(self):
+    """load config from config"""
+    with open('config') as f:
+        __raw_data = f.read()
+    
+    self.CONFIG = eval(__raw_data, {'__builtins__': None}, None)
+    
+    # listen
+    self.LISTEN_IP            = self.CONFIG['ip']
+    self.LISTEN_PORT          = self.CONFIG['local_port']
+    self.LISTEN_VISIBLE       = self.CONFIG['visible']
+    self.LISTEN_DEBUGINFO     = self.CONFIG['debuginfo'] if self.CONFIG.has_key('debuginfo') else 0
+    
+    # server info
+    self.SOCKS5_SERVER           = self.CONFIG['server']
+    self.SOCKS5_SERVER_PORT      = self.CONFIG['server_port']
+    self.SOCKS5_PASSWORD         = self.CONFIG['password']
+    self.SOCKS5_ENCRYPT_METHOD   = self.CONFIG['method']
 
-    def __init__(self):
-        """load config from config"""
-        with open('config') as f:
-            __raw_data = f.read()
-
-        self.CONFIG = eval(__raw_data, {'__builtins__': None}, None)
-
-        # listen
-        self.LISTEN_IP            = self.CONFIG['ip']
-        self.LISTEN_PORT          = self.CONFIG['local_port']
-        self.LISTEN_VISIBLE       = self.CONFIG['visible']
-        self.LISTEN_DEBUGINFO     = self.CONFIG['debuginfo'] if self.CONFIG.has_key('debuginfo') else 0
-
-        # server info
-        self.SOCKS5_SERVER           = self.CONFIG['server']
-        self.SOCKS5_SERVER_PORT      = self.CONFIG['server_port']
-        self.SOCKS5_PASSWORD         = self.CONFIG['password']
-        self.SOCKS5_ENCRYPT_METHOD   = self.CONFIG['method']
-
-        # app version
-        self.VERSION = __version__
-
-    def info(self):
-        info = ''
-        info += '------------------------------------------------------\n'
-        info += 'ShadowSocks Version    : %s (python/%s)\n' % (self.VERSION, sys.version.partition(' ')[0])
-        info += 'Listen Address         : %s:%d\n' % (self.LISTEN_IP, self.LISTEN_PORT)
-        info += 'Debug INFO             : %s\n' % self.LISTEN_DEBUGINFO if self.LISTEN_DEBUGINFO else ''
-        info += 'SOCKS5 Server          : %s:%d\n' % (self.SOCKS5_SERVER, self.SOCKS5_SERVER_PORT)
-        info += '------------------------------------------------------\n'
-        return info
+    self.TIMEOUT = self.CONFIG['timeout']
+    
+    # app version
+    self.VERSION = __version__
+  
+  def info(self):
+    info = ''
+    info += '------------------------------------------------------\n'
+    info += 'ShadowSocks Version    : %s (python/%s)\n' % (self.VERSION, sys.version.partition(' ')[0])
+    info += 'Listen Address         : %s:%d\n' % (self.LISTEN_IP, self.LISTEN_PORT)
+    info += 'Debug INFO             : %s\n' % self.LISTEN_DEBUGINFO if self.LISTEN_DEBUGINFO else ''
+    info += 'SOCKS5 Server          : %s:%d\n' % (self.SOCKS5_SERVER, self.SOCKS5_SERVER_PORT)
+    info += 'TimeOut                : %d\n' % self.TIMEOUT
+    info += '------------------------------------------------------\n'
+    return info
